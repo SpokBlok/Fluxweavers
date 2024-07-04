52f5070a8a4f10e8be16abf3e50f260d35c696d2
Kasumirre <105653241+Kasumirre@users.noreply.github.com>
2024-07-04 17:55:11 +0800

    Adjust the healthbar to subtract from the HP and not add by swapping + and -.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -17,9 +17,10 @@ public class HealthBar : MonoBehaviour
 
     public float TotalDamageReceived(float damage, float attackStat, float resistStat, float penetrationStat) // How much damage the player receives. ResistStat comes from you, and the other stats come from the damage dealer.
     {
+        totalDamage = 0;
         damage = damage / 100; //Converting damage to be multiplied by percentage of attack as seen in the aspirant stats
         float totalResist = resistStat - penetrationStat; 
-        totalDamage -= (damage * attackStat) / totalResist;
+        totalDamage += (damage * attackStat) / totalResist;
         return totalDamage;
     }
 }
