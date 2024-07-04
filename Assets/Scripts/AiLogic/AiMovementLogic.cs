e1cc8645182951b848fa48b0d970a0440fe412c9
Washington99 <118784933+Washington99@users.noreply.github.com>
2024-07-04 15:42:52 +0800

    Update AiMovementLogic.cs
    Prevented enemy from moving into player aspirant tile if they are already adjacent to the aspirant

--- Assets/Scripts/AiLogic/AiMovementLogic.cs
+++ Assets/Scripts/AiLogic/AiMovementLogic.cs
@@ -11,8 +11,8 @@ public class AiMovementLogic : MonoBehaviour
 
     private Vector3 moveX;
     private Vector3 moveY;
-    [SerializeField] private int remainingMoves;
-    private Vector3[] adjancenctTiles;
+    [SerializeField] private int remainingMoves; // For debugging. Don't remove me yet >_>
+    private Vector3[] adjancentTiles;
 
     // Start is called before the first frame update
     void Start()
@@ -21,21 +21,20 @@ public class AiMovementLogic : MonoBehaviour
         moveY = new Vector3(0.5f, 0.9f, 0f); // Change depending on hex size and dimensions
         remainingMoves = 0; // Don't move at start
 
-        adjancenctTiles = new Vector3[6];
-
-        adjancenctTiles[0] = moveX;
-        adjancenctTiles[1] = moveY;
-        adjancenctTiles[2] = -moveX + moveY;
-        adjancenctTiles[3] = -moveX;
-        adjancenctTiles[4] = -moveY;
-        adjancenctTiles[5] = moveX - moveY;
-
+        adjancentTiles = new Vector3[] { // Arranged in counter-clockwise order
+            moveX, moveY, -moveX + moveY, -moveX, -moveY, moveX - moveY
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (remainingMoves > 0) {
+
+        if (AdjancentTo(aspirant)) { // Change aspirant depending on AI targeting priority
+            remainingMoves = 0; // Should be changed to simply disallow movement to aspirant tile // <- TODO
+        }
+
+        if (remainingMoves > 0) { 
             // StartCoroutine(Move(moveSpeed * Time.deltaTime));
             Move(moveSpeed * Time.deltaTime);
         }
@@ -45,6 +44,8 @@ public class AiMovementLogic : MonoBehaviour
         }
     }
 
+
+    // Move should be updated eventually to take in ANY set of position vectors as parameter // <- TODO
     private void Move(float speed) {
         Vector3 enemy_position = gameObject.transform.position;
 
@@ -79,4 +80,13 @@ public class AiMovementLogic : MonoBehaviour
         // yield return new WaitForSeconds(1);
     }
 
+    private bool AdjancentTo (Transform targetAspirant) {
+        foreach (Vector3 tile in adjancentTiles) {
+            if (targetAspirant.position == gameObject.transform.position + tile) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
