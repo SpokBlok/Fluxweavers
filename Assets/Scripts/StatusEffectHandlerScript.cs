e80ec6efcea63c68a61a6f1d436ddadae2ee4ca4
Migs Atienza <160925292+SpokBlok@users.noreply.github.com>
2024-07-10 18:07:50 +0800

    Updated statusEffect manager to have list of effects and decrement their durations with each roundUpdate call

--- Assets/Scripts/StatusEffectHandlerScript.cs
+++ Assets/Scripts/StatusEffectHandlerScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class StatusEffectHandlerScript : MonoBehaviour
 {
-    int currentRound;
+    List<StatusEffect> effectList = new List<StatusEffect>();
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +18,22 @@ public class StatusEffectHandlerScript : MonoBehaviour
         
     }
 
-    public void addStatusEffect(string statusEffectName, float statusEffect, int duration, PlayerObject[] targets)
+    public void addStatusEffect(StatusEffect effect)
     {
-        FieldInfo stat = this.GetType().GetField(statusEffectName);
-        foreach (PlayerObject target in targets) 
+        effectList.Add(effect); //the effect is added to the Status Effect List
+    }
+
+    public void roundUpdate()
+    {
+        foreach (StatusEffect effect in effectList)
         {
+            effect.duration--; //The duration variable of each effect in the list is reduced
+            if (effect.duration == 0)
+            {
+                effect.revertEffect(); //Status effect is reverted
+                effectList.Remove(effect); //Effect is removed from the list
+                Destroy(effect);//Effect object is destroyed
+            }
         }
     }
 }
