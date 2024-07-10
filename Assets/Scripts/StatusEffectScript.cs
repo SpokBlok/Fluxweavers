3c1fa142000d205dfb471bdd1cbd332e869858b0
Migs Atienza <160925292+SpokBlok@users.noreply.github.com>
2024-07-10 17:17:13 +0800

    Apply & Revert effects in StatusEffect.script

---
+++ Assets/Scripts/StatusEffectScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class StatusEffect : MonoBehaviour
+{
+    public string statusEffectName;
+    public float statusEffect;
+    public int duration;
+    public PlayerObject[] targets;
+    public FieldInfo statField;
+    public float newStat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void instantiate(string statusEffectName, float statusEffect, int duration, PlayerObject[] targets)
+    {
+        this.statusEffectName = statusEffectName;
+        this.statusEffect = statusEffect;
+        this.duration = duration;
+        this.targets = targets;
+        applyEffect();
+
+    }
+
+    public void applyEffect()
+    {
+        foreach (PlayerObject target in targets)
+        {
+            statField = target.GetType().GetField(statusEffectName);
+            float oldStat = (float)statField.GetValue(target);
+            newStat = oldStat * statusEffect;
+            statField.SetValue(target, newStat);
+        }
+    }
+
+    public void revertEffect()
+    {
+
+        foreach (PlayerObject target in targets)
+        {
+            statField = target.GetType().GetField(statusEffectName);
+            float oldStat = (float)statField.GetValue(target);
+            newStat = oldStat / statusEffect;
+            statField.SetValue(target, newStat);
+        }
+    }
+}
