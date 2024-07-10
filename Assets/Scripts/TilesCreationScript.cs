9623305ba70aa75a77c1134a0c436610c51f4e4b
Nate Brevin Que <nate.que@obf.ateneo.edu>
2024-07-10 17:18:57 +0800

    Reworked the indexing of the tiles in the 2d array

--- Assets/Scripts/TilesCreationScript.cs
+++ Assets/Scripts/TilesCreationScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,6 +17,7 @@ public class TilesCreationScript : MonoBehaviour
         // format them into a grid
         int rowNumber = -1;
         int colNumber = 0;
+        int offsetX = 5;
 
         foreach (Transform row in mapTransform)
         {
@@ -23,13 +25,25 @@ public class TilesCreationScript : MonoBehaviour
                 break;
 
             rowNumber++;
-            colNumber = AddLeftEdgeTiles(rowNumber);
+            colNumber = AddLeftEdgeTiles(rowNumber, offsetX);
+
+            if(offsetX > 0)
+                offsetX--;
 
             for (int i = row.childCount - 1; i > -1; i--)
             {
                 Transform tile = row.GetChild(i);
 
                 Tiles[rowNumber, colNumber] = tile.gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber;
+                }
+                catch(Exception){}
+
                 colNumber++;
             }
 
@@ -37,84 +51,213 @@ public class TilesCreationScript : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    int AddLeftEdgeTiles(int rowNumber)
+    int AddLeftEdgeTiles(int rowNumber, int offsetX)
     {
         if (rowNumber > 0 && rowNumber < 10)
         {
             Transform LeftEdge = GameObject.Find("Left Edge").transform;
 
-            int colNumber = 0;
+            int colNumber = offsetX;
 
             if (rowNumber == 1)
             {
-                Tiles[1, 0] = LeftEdge.GetChild(0).gameObject;
-                colNumber = 1;
+                Tiles[1, colNumber] = LeftEdge.GetChild(0).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber;
+                }
+                catch(Exception){}
+
+                colNumber += 1;
             }
 
             else if (rowNumber == 2)
             {
-                Tiles[2, 0] = LeftEdge.GetChild(5).gameObject;
-                colNumber = 1;
+                Tiles[2, colNumber] = LeftEdge.GetChild(5).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber;
+                }
+                catch(Exception){}
+                
+                colNumber += 1;
             }
 
             else if (rowNumber == 3)
             {
-                Tiles[3, 0] = LeftEdge.GetChild(9).gameObject;
-                Tiles[3, 1] = LeftEdge.GetChild(1).gameObject;
-                colNumber = 2;
+                Tiles[3, colNumber] = LeftEdge.GetChild(9).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber;
+                }
+                catch(Exception){}
+                
+                Tiles[3, colNumber + 1] = LeftEdge.GetChild(1).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber+1;
+                }
+                catch(Exception){}
+                
+                colNumber += 2;
             }
 
             else if (rowNumber == 4)
             {
-                Tiles[4, 0] = LeftEdge.GetChild(12).gameObject;
-                Tiles[4, 1] = LeftEdge.GetChild(6).gameObject;
-                colNumber = 2;
+                Tiles[4, colNumber] = LeftEdge.GetChild(12).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber;
+                }
+                catch(Exception){}
+                
+                Tiles[4, colNumber + 1] = LeftEdge.GetChild(6).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber+1;
+                }
+                catch(Exception){}
+                
+                colNumber += 2;
             }
 
             else if (rowNumber == 5)
             {
-                Tiles[5, 0] = LeftEdge.GetChild(14).gameObject;
-                Tiles[5, 1] = LeftEdge.GetChild(10).gameObject;
-                Tiles[5, 2] = LeftEdge.GetChild(2).gameObject;
-                colNumber = 3;
+                Tiles[5, colNumber] = LeftEdge.GetChild(14).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber;
+                }
+                catch(Exception){}
+                
+                Tiles[5, colNumber + 1] = LeftEdge.GetChild(10).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber+1;
+                }
+                catch(Exception){}
+                
+                Tiles[5, colNumber + 2] = LeftEdge.GetChild(2).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber+2;
+                }
+                catch(Exception){}
+                
+                colNumber += 3;
             }
 
             else if (rowNumber == 6)
             {
-                Tiles[6, 0] = LeftEdge.GetChild(13).gameObject;
-                Tiles[6, 1] = LeftEdge.GetChild(7).gameObject;
-                colNumber = 2;
+                Tiles[6, colNumber] = LeftEdge.GetChild(13).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber;
+                }
+                catch(Exception){}
+                
+                Tiles[6, colNumber + 1] = LeftEdge.GetChild(7).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber+1;
+                }
+                catch(Exception){}
+                
+                colNumber += 2;
             }
 
             else if (rowNumber == 7)
             {
-                Tiles[7, 0] = LeftEdge.GetChild(11).gameObject;
-                Tiles[7, 1] = LeftEdge.GetChild(3).gameObject;
-                colNumber = 2;
+                Tiles[7, colNumber] = LeftEdge.GetChild(11).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber;
+                }
+                catch(Exception){}
+                
+                Tiles[7, colNumber + 1] = LeftEdge.GetChild(3).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber+1;
+                }
+                catch(Exception){}
+                
+                colNumber += 2;
             }
 
             else if (rowNumber == 8)
             {
-                Tiles[8, 0] = LeftEdge.GetChild(8).gameObject;
-                colNumber = 1;
+                Tiles[8, colNumber] = LeftEdge.GetChild(8).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber;
+                }
+                catch(Exception){}
+                
+                colNumber += 1;
             }
 
             else if (rowNumber == 9)
             {
-                Tiles[9, 0] = LeftEdge.GetChild(4).gameObject;
-                colNumber = 1;
+                Tiles[9, colNumber] = LeftEdge.GetChild(4).gameObject;
+
+                try
+                {
+                    TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                    indices.y = rowNumber;
+                    indices.x = colNumber;
+                }
+                catch(Exception){}
+                
+                colNumber += 1;
             }
 
             return colNumber;
         }
 
-        return 0;
+        return offsetX;
     }
 
     void AddRightEdgeTiles(int rowNumber, int colNumber)
@@ -122,61 +265,247 @@ public class TilesCreationScript : MonoBehaviour
         Transform RightEdge = GameObject.Find("Right Edge").transform;
 
         if (rowNumber == 0)
+        {
             Tiles[0, colNumber] = RightEdge.GetChild(0).gameObject;
 
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber;
+            }
+            catch(Exception){}       
+        }
+
         else if (rowNumber == 1)
+        {
             Tiles[1, colNumber] = RightEdge.GetChild(6).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber;
+            }
+            catch(Exception){}    
+        }
 
         else if (rowNumber == 2)
         {
             Tiles[2, colNumber] = RightEdge.GetChild(1).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber;
+            }
+            catch(Exception){}    
+
             Tiles[2, colNumber] = RightEdge.GetChild(11).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber+1;
+            }
+            catch(Exception){}    
         }
 
         else if (rowNumber == 3)
         {
             Tiles[3, colNumber] = RightEdge.GetChild(7).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber;
+            }
+            catch(Exception){}    
+
             Tiles[3, colNumber + 1] = RightEdge.GetChild(15).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber+1;
+            }
+            catch(Exception){}    
         }
 
         else if (rowNumber == 4)
         {
             Tiles[4, colNumber] = RightEdge.GetChild(2).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber;
+            }
+            catch(Exception){}    
+
             Tiles[4, colNumber + 1] = RightEdge.GetChild(12).gameObject;
-            Tiles[4, colNumber + 1] = RightEdge.GetChild(18).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber+1;
+            }
+            catch(Exception){}    
+
+            Tiles[4, colNumber + 2] = RightEdge.GetChild(18).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber+2;
+            }
+            catch(Exception){}    
         }
 
         else if (rowNumber == 5)
         {
             Tiles[5, colNumber] = RightEdge.GetChild(8).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber;
+            }
+            catch(Exception){}    
+
             Tiles[5, colNumber + 1] = RightEdge.GetChild(16).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber+1;
+            }
+            catch(Exception){}    
+
             Tiles[5, colNumber + 2] = RightEdge.GetChild(20).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber+2;
+            }
+            catch(Exception){}    
         }
 
         else if (rowNumber == 6)
         {
             Tiles[6, colNumber] = RightEdge.GetChild(3).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber;
+            }
+            catch(Exception){}
+
             Tiles[6, colNumber + 1] = RightEdge.GetChild(13).gameObject;
-            Tiles[6, colNumber + 1] = RightEdge.GetChild(19).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber+1;
+            }
+            catch(Exception){}    
+
+            Tiles[6, colNumber + 2] = RightEdge.GetChild(19).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber+2;
+            }
+            catch(Exception){}    
         }
 
         else if (rowNumber == 7)
         {
             Tiles[7, colNumber] = RightEdge.GetChild(9).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber;
+            }
+            catch(Exception){}    
+
             Tiles[7, colNumber + 1] = RightEdge.GetChild(17).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber+1;
+            }
+            catch(Exception){}    
         }
 
         else if (rowNumber == 8)
         {
             Tiles[8, colNumber] = RightEdge.GetChild(4).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber;
+            }
+            catch(Exception){}    
+
             Tiles[8, colNumber] = RightEdge.GetChild(14).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber+1;
+            }
+            catch(Exception){}    
         }
 
         else if (rowNumber == 9)
+        {
             Tiles[9, colNumber] = RightEdge.GetChild(10).gameObject;
 
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber;
+            }
+            catch(Exception){}    
+        }
+
         else if (rowNumber == 10)
+        {
             Tiles[10, colNumber] = RightEdge.GetChild(5).gameObject;
+
+            try
+            {
+                TileIndices indices = Tiles[rowNumber, colNumber].GetComponent<TileIndices>();
+                indices.y = rowNumber;
+                indices.x = colNumber;
+            }
+            catch(Exception){}    
+        }
     }
 
     public int returnRowCount()
---
+++ Assets/TileIndices.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileIndices : MonoBehaviour
+{
+    public int y;
+    public int x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
